2e7cd9e83457eb002dd93ed3514f8c993fec421d
Craig Wayne <5500952+craigiswayne@users.noreply.github.com>
2025-10-29 16:50:07 +0000

    Started Theme Customizer Component (#5)

---
+++ projects/component-library/src/theme-customizer/theme-customizer.component.ts
@@ -0,0 +1,12 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'lib-theme-customizer-trigger',
+  imports: [],
+  templateUrl: './theme-customizer.component.html',
+  styleUrl: './theme-customizer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ThemeCustomizerComponent {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {PillComponent, PillGroupComponent} from 'component-library';
+import {PillComponent, PillGroupComponent, ThemeCustomizerComponent} from 'component-library';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PillComponent, PillGroupComponent],
+  imports: [RouterOutlet, PillComponent, PillGroupComponent, ThemeCustomizerComponent],
   templateUrl: './app.html',
 })
 export class App {
