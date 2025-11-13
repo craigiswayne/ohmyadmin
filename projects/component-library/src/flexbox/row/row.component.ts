04293a19dac30f74d813ac7f3d85fec3d1e0d0f4
Craig Wayne <craigiswayne@gmail.com>
2025-11-13 09:10:50 +0000

    Justify Content supported on flexbox row

--- projects/component-library/src/flexbox/row/row.component.ts
+++ projects/component-library/src/flexbox/row/row.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component, computed, input} from '@angular/core
 
 type SupportedDirections = 'row' | 'column' | 'row-reverse' | 'column-reverse';
 type SupportedAlignItems = | 'stretch' | 'flex-start' | 'flex-end' | 'center' | 'baseline';
+type SupportedJustifyContent = | 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | 'stretch';
 
 @Component({
   selector: 'row',
@@ -12,15 +13,17 @@ type SupportedAlignItems = | 'stretch' | 'flex-start' | 'flex-end' | 'center' | 
     class: 'row',
     '[style.--direction]': 'direction()',
     '[style.--gap]': 'item_gap_parsed()',
-    '[style.--flex-style]': 'inline() ? "inline-flex" : "flex"',
+    '[style.--flex-style]': 'inline() ? "inline-flex" : null',
     '[style.--align-items]': 'align_items()',
+    '[style.--justify-content]': 'justify_content()',
   }
 })
 export class RowComponent {
-  gap = input<string|number>(0)
-  direction = input<SupportedDirections>('row')
-  inline = input<boolean>(false)
-  align_items = input<SupportedAlignItems>('stretch', {alias: 'align-items'})
+  gap = input<string | number>()
+  direction = input<SupportedDirections>()
+  inline = input<boolean>()
+  align_items = input<SupportedAlignItems|null>(null, {alias: 'align-items'})
+  justify_content = input<SupportedJustifyContent|null>(null, {alias: 'justify-content'})
 
   item_gap_parsed = computed(() => {
     const val = this.gap();
