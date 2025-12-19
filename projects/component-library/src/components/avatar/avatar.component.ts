88b85235173ffd332355082be6d21e18558b2491
Craig Wayne <craigiswayne@gmail.com>
2025-12-19 10:58:02 +0000

    Avtar Component Updates

--- projects/component-library/src/components/avatar/avatar.component.ts
+++ projects/component-library/src/components/avatar/avatar.component.ts
@@ -7,7 +7,7 @@ import {TAILWIND_SIZES} from '../../types/tailwind-sizes.type';
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss',
   host: {
-    '[style.--border-width]': `bordered() ? '3px' : undefined`,
+    '[style.--border-width]': `bordered() ? '0.2em' : undefined`,
     '[style.--background-color]': 'random_milky_hsl_color',
     '[style.--size]': 'css_size_variable()'
   },
@@ -18,9 +18,9 @@ export class AvatarComponent {
   image_url = input<string>();
   name = input<string>();
   size = input<TAILWIND_SIZES>()
-  status = input<'online'|'offline'>()
+  status = input<'online' | 'offline'>()
 
-  protected css_size_variable = computed<string|undefined>(() => {
+  protected css_size_variable = computed<string | undefined>(() => {
     const size = this.size();
     return size === undefined ? undefined : `var(--size-${size})`
   })
@@ -28,11 +28,11 @@ export class AvatarComponent {
   protected readonly random_milky_hsl_color = random_milky_hsl_color();
 
   protected initials = computed<string>(() => {
-      return (this.name() ?? 'Unknown User')
-        .split(/\s/)
-        .filter(i => i)
-        .map((part: string) => part[0].toUpperCase())
-        .slice(0, 2).join('');
+    return (this.name() ?? 'Unknown User')
+      .split(/\s/)
+      .filter(i => i)
+      .map((part: string) => part[0].toUpperCase())
+      .slice(0, 2).join('');
   })
 
   protected image_load_error = signal<boolean>(false);
---
+++ projects/component-library/src/types/tailwind-sizes.enum.ts
@@ -0,0 +1,15 @@
+export const enum TAILWIND_SIZES_ENUM {
+  XXXS = '3xs',
+  XXS = '2xs',
+  EXTRA_SMALL = 'xs',
+  SMALL = 'sm',
+  MEDIUM = 'md',
+  LARGE = 'lg',
+  EXTRA_LARGE = 'xl',
+  XXL = '2xl',
+  XXXL = '3xl',
+  XXXXL = '4xl',
+  XXXXXL = '5xl',
+  XXXXXXL = '6xl',
+  XXXXXXXL = '7xl'
+}
